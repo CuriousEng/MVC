add3140031644b0d235f6ea317a160d6bb4b53b2
CuriousEngineer <kirill.s.works@gmail.com>
2019-07-01 14:46:43 +0300

    Added model package

---
+++ src/com/company/model/FakeModel.java
@@ -0,0 +1,24 @@
+package com.company.model;
+
+import com.company.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("Ivan",1, 1));
+        list.add(new User("Fedor",2, 3));
+        list.add(new User("Kirill",3, 80));
+        modelData.setUsers(list);
+    }
+}
---
+++ src/com/company/model/ModelData.java
@@ -0,0 +1,18 @@
+package com.company.model;
+
+import com.company.bean.User;
+
+import java.util.List;
+
+public class ModelData {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
+
