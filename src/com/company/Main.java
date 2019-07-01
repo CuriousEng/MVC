a95abc4e51f303d52fa76d0b84fdb3b5e9551807
CuriousEngineer <kirill.s.works@gmail.com>
2019-07-01 19:57:53 +0300

    User edition function is finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,25 @@ import com.company.controller.Controller;
 import com.company.model.FakeModel;
 import com.company.model.MainModel;
 import com.company.model.Model;
+import com.company.view.EditUserView;
 import com.company.view.UsersView;
 
 public class Main {
     public static void main(String[] args) {
         Model model = new MainModel();
         UsersView usersView = new UsersView();
+        EditUserView editUserView =new EditUserView();
         Controller controller = new Controller();
+        controller.setEditUserView(editUserView);
 
         usersView.setController(controller);
         controller.setModel(model);
         controller.setUsersView(usersView);
 
         usersView.fireEventShowAllUsers();
+
+        usersView.fireEventOpenUserEditForm(126);
+
         usersView.fireEventShowDeletedUsers();
     }
 }
